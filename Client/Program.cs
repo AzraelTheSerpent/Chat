47718ef55d073b07a855e06a9e32dccda9d5043f
AzraelTheSerpent <147224167+AzraelTheSerpent@users.noreply.github.com>
2024-04-21 15:49:03 +0300

    Chat v0.1

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,10 +1,75 @@
-﻿namespace Client;
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+using System.Threading.Tasks;
 
-class Program
+string host = "127.0.0.1";
+int port = 8888;
+
+TcpClient client = new();
+StreamReader? reader = null;
+StreamWriter? writer = null;
+string? userName = null;
+
+try
 {
-    public static void Main()
+    client.Connect(host, port);
+    
+    reader = new StreamReader(client.GetStream());
+    writer = new StreamWriter(client.GetStream());
+    if (reader is null || writer is null) return;
+    
+    Console.Write("Ведите свое имя: ");
+    userName = Console.ReadLine();
+
+    Task ReceiveMsg = ReceiveMessageAsync(reader);
+    Task SendMsg = SendMassageAsync(writer);
+
+    Task.WaitAny(ReceiveMsg, SendMsg);
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+client.Close();
+writer?.Close();
+reader?.Close();
+    
+async Task SendMassageAsync(StreamWriter writer)
+{
+    await writer.WriteLineAsync(userName);
+    await writer.FlushAsync();
+
+    Console.Clear();
+    Console.WriteLine(new String('#', Console.WindowWidth));
+    
+    while (true)
     {
+        string? message = Console.ReadLine();
+
+        if (string.IsNullOrEmpty(message)) continue;
+
+        await writer.WriteLineAsync(message);
+        await writer.FlushAsync();
         
+        if (message.Equals("/exit")) break;
     }
 }
 
+async Task ReceiveMessageAsync(StreamReader reader)
+{
+    while (true)
+    {
+        try
+        {
+            string? message = await reader.ReadLineAsync();
+
+            if (string.IsNullOrEmpty(message)) continue;
+            Console.WriteLine(message);
+        }
+        catch
+        {
+            break;
+        }
+    }
+}
--- Server/Program.cs
+++ Server/Program.cs
@@ -1,9 +1,140 @@
-﻿namespace Server;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
 
-class Program
+
+ServerObject server = new();
+await server.ListenAsync();
+
+class ServerObject
 {
-    public static void Main()
+    protected internal TcpListener Listener = new(IPAddress.Any, 8888);
+    protected internal List<ClientObject> clients = new();
+
+    protected internal async Task ListenAsync()
     {
+        try
+        {
+            Listener.Start();
+            Console.WriteLine("Сервер запущен. Ожидание подключений...");
+
+            while (true)
+            {
+                TcpClient client = await Listener.AcceptTcpClientAsync();
+
+                ClientObject clientObject = new(client, this);
+                clients.Add(clientObject);
+                Task tmpTask = clientObject.StartAsync();
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        finally
+        {
+            Disconnect();
+        }
+    }
+
+    protected internal async Task BroadcastMessageAsync(string message, string id)
+    {
+        foreach (var client in clients)
+            if (client.Id != id)
+            {
+                await client.Writer.WriteLineAsync(message);
+                await client.Writer.FlushAsync();
+            }
+    }
+
+    private void Disconnect()
+    {
+        foreach(ClientObject client in clients)
+            client.Close();
+        Listener.Stop();
+    }
+
+    protected internal void RemoveConnection(string id)
+    {
+        ClientObject? client = clients.FirstOrDefault(c => c.Id == id);
+
+        if (client != null) clients.Remove(client);
+        client?.Close();
+    }
+}
+
+class ClientObject
+{
+    protected internal string Id {get;} = Guid.NewGuid().ToString();
+    protected internal StreamWriter Writer {get;}
+    protected internal StreamReader Reader {get;}
+    private TcpClient _client;
+    private ServerObject _server;
+
+    public ClientObject(TcpClient client, ServerObject server)
+    {
+        _client = client;
+        _server = server;
+
+        var steam = client.GetStream();
         
+        Writer = new StreamWriter(steam);
+        Reader = new StreamReader(steam);
+    }
+
+    public async Task StartAsync()
+    {
+        try
+        {
+            string? userName = await Reader.ReadLineAsync();
+            string? message = $"{userName} вошел в чат";
+            
+            Console.WriteLine($"User: {userName}\nId: {Id}\nMessage: {message}\n");
+            await _server.BroadcastMessageAsync(message, Id);
+
+            while (true)
+            {
+                try
+                {
+                    message = await Reader.ReadLineAsync();
+                    
+                    if (message == null) continue;
+                    if (message.Equals("/exit")) throw new Exception();
+                    
+                    Console.WriteLine($"User: {userName}\nId: {Id}\nMessage: {message}\n");
+                    
+                    message = $"{userName}: {message}";
+                    await _server.BroadcastMessageAsync(message, Id);
+                }
+                catch
+                {
+                    message = $"{userName} покинул чат";
+
+                    Console.WriteLine($"User: {userName}\nId: {Id}\nMessage: {message}\n");
+
+                    await _server.BroadcastMessageAsync(message, Id);
+                    break;
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        finally
+        {
+            _server.RemoveConnection(Id);
+        }
+    }
+
+    protected internal void Close()
+    {
+        Writer.Close();
+        Reader.Close();
+        _client.Close();
     }
 }
